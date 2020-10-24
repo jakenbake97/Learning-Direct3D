25c0643f5674a745a1e1bfa0f881907e17f92546
Jake <jakenbake97@gmail.com>
2020-10-23 23:18:25 -0500

    Move window creation to a dedicated class, along with message handler

---
+++ Hardware3D/Hardware3D/Window.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "WinInclude.h"
+
+class Window
+{
+private:
+	// singleton for managing the registration and cleanup of window class
+	class WindowClass
+	{
+	public:
+		static const char* GetName() noexcept;
+		static HINSTANCE GetInstance() noexcept;
+	private:
+		WindowClass() noexcept;
+		~WindowClass();
+		WindowClass(const WindowClass&) = default;
+		WindowClass& operator=(const WindowClass&) = delete;
+		
+		static constexpr const char* wndClassName = "Direct3D Engine Window";
+		static WindowClass wndClass;
+		HINSTANCE hInst;
+	};
+public:
+	Window(int width, int height, const char* name) noexcept;
+	~Window();
+	Window(const Window&) = delete;
+	Window& operator=(const Window&) = delete;
+private:
+	static LRESULT CALLBACK HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	static LRESULT CALLBACK HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+
+	
+	int width;
+	int height;
+	HWND hWnd;
+};
